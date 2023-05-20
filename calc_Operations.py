ab5316f21ce3d4a612da44fac70d5764ccb58ee0
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-05-20 10:46:40 +0800

    Added user input

--- calc_Operations.py
+++ calc_Operations.py
@@ -1,4 +1,5 @@
 class Operations:
     def choose_operations(self):
-    operations_list=["A", "S", "M", "D"]
-    
+        operations_list=["A", "S", "M", "D"]
+        user_operation=input("Please input an operation to use. Type the letters indicated for each operation: A for Addition, M for multiplication, S for subtraction, D for division: ")
+        user_operation=user_operation.upper()
