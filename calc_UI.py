bd72730b6a2678e1f76d1cc86d8a51043f472d08
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-05-21 21:00:15 +0800

    Fixed the onstamce of opening multiple windows in the code when clicking a button

--- calc_UI.py
+++ calc_UI.py
@@ -1,31 +1,34 @@
 import tkinter as tk
 import pyfiglet
 class UI:
+    def __init__(self):
+        self.window=tk.Tk()
+
     def quit_window(self):
-        window=tk.Tk()
-        window.quit()
+        self.window.quit()
 
     def initial_screen(self):
-        window=tk.Tk()
-        window.minsize(500,300)
-        window.title("Calculator")
-        label=tk.Label(window, text=pyfiglet.figlet_format("Calculator", justify="center", font="slant"))
+        self.window.minsize(500,300)
+        self.window.title("Calculator")
+        label=tk.Label(self.window, text=pyfiglet.figlet_format("Calculator", justify="center", font="slant"))
         label.pack()
-        label2=tk.Label(window, text="Welcome to the calculator made with OOP. Made by Pierre Victor T. Zurbito")
+        label2=tk.Label(self.window, text="Welcome to the calculator made with OOP. Made by Pierre Victor T. Zurbito")
         label2.pack()
-        button=tk.Button(window, text="Take me in!", command=self.quit_window)
+        button=tk.Button(self.window, text="Take me in!", command=self.operation_selection)
         button.pack()
-        window.mainloop()
 
     def operation_selection(self):
-        window=tk.Tk()
-        window.minsize(500,300)
-        window.title("Calculator")
-        label=tk.Label(window, text="Please choose your operation from the dropdown box.")
+        self.window.destroy()
+        operation_window=tk.Tk()
+        self.window.minsize(500,300)
+        self.window.title("Calculator")
+        label=tk.Label(self.window, text="Please choose your operation from the dropdown box.")
         label.pack()
-        window.mainloop()
+
+    def run(self):
+        self.window.mainloop()
         
 
 ui=UI()
 ui.initial_screen()
-ui.operation_selection()
+ui.run()
