a2e0fa47f2f1a62eabe7a2fffe7a337d12080651
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-05-22 01:02:29 +0800

    Creates loop mechanism

--- calc_Operations.py
+++ calc_Operations.py
@@ -1,20 +1,18 @@
-from calc_UI import ui
 class Operations:
     def choose_operations(self):
         while True:
             operations_list=["A", "S", "M", "D"]
-            global user_operation
-            user_operation=input("Please input an operation to use. Type the letters indicated for each operation: A for Addition, M for multiplication, S for subtraction, D for division: ")
-            user_operation=user_operation.upper()
-            if user_operation in operations_list:
-                print('You chose ' + user_operation)
+            global chosen_operation
+            chosen_operation=input("Please input an operation to use. Type the letters indicated for each operation: A for Addition, M for multiplication, S for subtraction, D for division: ")
+            chosen_operation=chosen_operation.upper()
+            if chosen_operation in operations_list:
+                print('You chose ' + chosen_operation)
                 break
             else:
                 print("Not a valid operation! Please try again.")
-
     def input_numbers(self):
-        global operand_one, operand_two
         while True:
+            global operand_one, operand_two
             try:
                 operand_one=float(input("Please input your first number: "))
                 break
@@ -27,9 +25,9 @@ class Operations:
             except ValueError:
                 print("Operand must be a number! Please try again.")
 
+
     def operations_proper(self):
         global operand_one, operand_two
-        chosen_operation=ui.operation_selection.dropdown.get()
         if chosen_operation=="A":
             sum=operand_one + operand_two
             print("The sum of the operands is " + str(sum))
@@ -47,3 +45,12 @@ class Operations:
                     break
                 except ZeroDivisionError:
                         operand_two=float(input("Your second operand cannot be zero! Input another number: "))
+    
+    def loop(self):
+        while True:
+            loop_options=['Y','N']
+            user_loop=input("Do you want to do another calculation? (Y/N)")
+            if loop_options=="Y":
+                
+            else:
+                break
--- calc_V2.py
+++ calc_V2.py
@@ -1,7 +1,7 @@
-from calc_UI import UI
 from calc_Operations import Operations
 
 Op=Operations()
 Op.choose_operations()
 Op.input_numbers()
 Op.operations_proper()
+Op.loop()
