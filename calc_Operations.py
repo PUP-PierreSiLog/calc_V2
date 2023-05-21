56696786a6f25da442e2bfeb07e1af309b832e64
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-05-22 01:45:12 +0800

    Added designs

--- calc_Operations.py
+++ calc_Operations.py
@@ -1,8 +1,10 @@
+import pyfiglet
 class Operations:
     def choose_operations(self):
         while True:
             operations_list=["A", "S", "M", "D"]
             global chosen_operation
+            print(pyfiglet.figlet_format("The calculator!", font="isometric2", justify="center"))
             chosen_operation=input("Please input an operation to use. Type the letters indicated for each operation: A for Addition, M for multiplication, S for subtraction, D for division: ")
             chosen_operation=chosen_operation.upper()
             if chosen_operation in operations_list:
@@ -46,7 +48,21 @@ class Operations:
                     break
                 except ZeroDivisionError:
                         operand_two=float(input("Your second operand cannot be zero! Input another number: "))
-    
+            #If division, asks the user if they want to see the whole number only or the remainder only
+            division_choices_list=["M", "NR"]
+            while True:
+                division_choices=input("To know the remainder of your equation, type M. If you want to know the result without any remainder, type NR.")
+                division_choices=division_choices.upper()
+                if division_choices in division_choices_list:
+                    break
+                else:
+                    print("Invalid answer! Please try again.")
+            if "M" in division_choices:
+                result=operand_one%operand_two
+                print(result)
+            if "NR" in division_choices:
+                result=operand_one//operand_two
+                print(result)
     def loop(self):
         while True:
             loop_options=['Y','N']
@@ -60,4 +76,4 @@ class Operations:
             elif "N" in user_loop:
                 break
             else:
-                user_loop=input("Invalid input! Please reinput (Y/N)")
+                print("Invalid input! Please reinput (Y/N)")
