c6328f6e6aed8db4e663441d8b5e9d22df7c5ece
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-06-03 22:27:25 +0800

    separated integer input to another file

--- calc_V2.py
+++ calc_V2.py
@@ -2,15 +2,17 @@ from calc_Operations import Operations
 from AdvancedDivisionCalculator import AdvancedDivisionCalculator
 from AdvancedDivisionCalculator_2 import RemainderOnly
 from calc_input import GetInput
+from calc_integer_input import IntegerInput
 ui=GetInput()
 no_remainder=AdvancedDivisionCalculator()
 basic_calculations=Operations()
 advanced_calculations=AdvancedDivisionCalculator()
 advanced_calculations_2=RemainderOnly()
+input_integer=IntegerInput
 
 chosen_operation = ui.choose_operations()
-operand_one=ui.input_number()
-operand_two=ui.input_number()
+operand_one=input_integer.input_number()
+operand_two=input_integer.input_number()
 basic_calculations.operations_proper(chosen_operation, operand_one, operand_two)
 advanced_calculations_2.remainder_only(chosen_operation, operand_one, operand_two)
 
---
+++ calc_integer_input.py
@@ -0,0 +1,4 @@
+class IntegerInput:
+    def input_number(self):
+        integer=float(input("Kindly input a number:"))
+        return integer
